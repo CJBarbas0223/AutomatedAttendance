0d0ba8638e646c5bfcb72d1aafcca60bfbf0cd58
Christian <christianjaybarbas@gmail.com>
2025-04-29 18:05:12 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './client/src/screens/students/Welcome';
+import StudentLogin from './client/src/screens/students/StudentLogin';
+import AdminLogin from './client/src/screens/admin/AdminLogin';
+import Dashboard from './client/src/screens/admin/Dashboard';
+import Signup from './client/src/screens/admin/Signup';
+import RoleSelection from './client/src/screens/students/RoleSelection';
+import StudentDashboard from './client/src/screens/students/StudentDashboard';
+import InstructorDashboard from './client/src/screens/instructor/InstructorDashboard';
+import InstructorLogin from './client/src/screens/instructor/InstructorLogin';
+import EnrolledStudent from './client/src/screens/instructor/EnrolledStudent';
+import { AuthProvider } from './client/src/context/AuthContext';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Welcome"
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Welcome" component={Welcome} />
+          <Stack.Screen name="StudentLogin" component={StudentLogin} />
+          <Stack.Screen name="AdminLogin" component={AdminLogin} />
+          <Stack.Screen name="InstructorLogin" component={InstructorLogin} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen name="RoleSelection" component={RoleSelection} />
+          <Stack.Screen name="StudentDashboard" component={StudentDashboard} />
+          <Stack.Screen name="InstructorDashboard" component={InstructorDashboard} />
+          <Stack.Screen name="EnrolledStudent" component={EnrolledStudent} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
